bbe8092d3448fde690899a9cc45089266310dbe2
codypy <code.codynoel@gmail.com>
2020-10-27 14:51:09 -0600

    Little cute surprise for the wife

---
+++ mamas.py
@@ -0,0 +1,9 @@
+print('How much do you love your Papas: ')
+
+x = input()
+
+if x == (""):
+    print('Please tell me how much you love me!')
+
+else:
+    print('Im glad you love me that much, but i will always love you more!')
